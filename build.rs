7275ec861278af9c44863ab3f3de35ffcfbdd4eb
nabijaczleweli <nabijaczleweli@gmail.com>
2018-11-23 07:16:45 +0100

    Compile and link vswhom

---
+++ build.rs
@@ -0,0 +1,17 @@
+extern crate cc;
+
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=ext/vswhom.cpp");
+
+    cc::Build::new()
+        .cpp(true)
+        .warnings(false)
+        .extra_warnings(false)
+        .file("ext/vswhom.cpp")
+        .compile("vswhom");
+
+    println!("cargo:rustc-link-lib=dylib=OleAut32");
+    println!("cargo:rustc-link-lib=dylib=Ole32");
+}
